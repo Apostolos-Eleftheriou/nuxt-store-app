048b7a216424727d0c5d6e152001f121b0e0f9f1
Apostolos Eleftheriou <95987435+Apostolos-Eleftheriou@users.noreply.github.com>
2024-07-13 16:24:08 +0300

    progress

---
+++ components/ProductCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card text-center">
+        <img :src="product.image" alt="product" class="w-full h-48 object-contain thumb">
+        <p class="text-gray-500 m-4 font-bold truncate">{{ product.title }}</p>
+        <NuxtLink :to="`/products/${product.id}`">
+            <p class="btn">View Details</p>
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup>
+const { product } = defineProps(['product'])
+</script>
+
+<style scoped>
+.thumb {
+    max-height: 120px;
+    max-width: 70%;
+    margin: 0 auto;
+}
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="grid grid-cols-4 gap-5">
+            <div v-for="p in products">
+                <ProductCard :product="p"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+// use prodcuts layout
+definePageMeta({
+    layout: 'products'
+})
+
+const { data: products} = await useFetch("https://fakestoreapi.com/products")
+
+// change the title and meta tags per component basis
+useHead({
+    title: 'Nuxt Store | Merch',
+    meta: [
+        {
+            name: 'description',
+            content: 'List of products'
+        }
+    ]
+})
+
+</script>
+
+<style scoped>
+
+</style>
